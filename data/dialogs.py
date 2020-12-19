3d6278d7d762ce1c58899c4b7165d9d23b7cd855
Jules <5582567+pondchamp@users.noreply.github.com>
2020-12-18 19:13:41 -0800

    alias

--- data/dialogs.py
+++ data/dialogs.py
@@ -1,11 +1,11 @@
 from typing import List, Optional
 
-from data.enums import ResponseFlags
+from data.enums import ResponseFlags as RF
 
 
 class Dialog:
     def __init__(self, text: str, min_turns: Optional[int] = None, max_turns: Optional[int] = None,
-                 flags: List[ResponseFlags] = None):
+                 flags: List[RF] = None):
         self.text = text
         self.max_turns = max_turns
         self.min_turns = min_turns
@@ -15,25 +15,25 @@ class Dialog:
 statement: List[Dialog] = [
     Dialog("hmm"),
     Dialog("ok"),
-    Dialog("[lb]", max_turns=0, flags=[ResponseFlags.BODY_FOUND_ME]),
+    Dialog("[lb]", max_turns=0, flags=[RF.BODY_FOUND_ME]),
     Dialog("[p] is clear"),
     Dialog("[p] is safe"),
-    Dialog("the body is in [lb]", max_turns=0, flags=[ResponseFlags.BODY_FOUND_ME]),
-    Dialog("I saw a vent but didn't see who", flags=[ResponseFlags.BODY_FOUND_ME, ResponseFlags.BODY_FOUND_OTHER]),
+    Dialog("the body is in [lb]", max_turns=0, flags=[RF.BODY_FOUND_ME]),
+    Dialog("I saw a vent but didn't see who", flags=[RF.BODY_FOUND_ME, RF.BODY_FOUND_OTHER]),
     Dialog("i saw [p] last"),
     Dialog("i saw [p] in [l]"),
 ]
 
 probe: List[Dialog] = [
-    Dialog("where was the body?", max_turns=0, flags=[ResponseFlags.BODY_FOUND_OTHER]),
+    Dialog("where was the body?", max_turns=0, flags=[RF.BODY_FOUND_OTHER]),
     Dialog("was anyone in [l]?"),
     Dialog("where was [p]?"),
     Dialog("who was in [l]?"),
-    Dialog("who did i just see?", max_turns=1, flags=[ResponseFlags.BODY_FOUND_ME]),
+    Dialog("who did i just see?", max_turns=1, flags=[RF.BODY_FOUND_ME]),
     Dialog("was anyone [t]?"),
-    Dialog("where?", max_turns=0, flags=[ResponseFlags.BODY_FOUND_OTHER]),
-    Dialog("??", max_turns=0, flags=[ResponseFlags.EMERGENCY_MEET_OTHER]),
-    Dialog("what?", max_turns=0, flags=[ResponseFlags.EMERGENCY_MEET_OTHER]),
+    Dialog("where?", max_turns=0, flags=[RF.BODY_FOUND_OTHER]),
+    Dialog("???", max_turns=0, flags=[RF.EMERGENCY_MEET_OTHER]),
+    Dialog("what?", max_turns=0, flags=[RF.EMERGENCY_MEET_OTHER]),
 ]
 
 attack: List[Dialog] = [
@@ -45,12 +45,12 @@ attack: List[Dialog] = [
     Dialog("[p] was faking their tasks"),
     Dialog("why weren't you doing task in [l]?", min_turns=1),
     Dialog("I'm voting [p]"),
-    Dialog("[p]", max_turns=0, flags=[ResponseFlags.BODY_FOUND_OTHER, ResponseFlags.BODY_FOUND_ME]),
-    Dialog("why was [p] near the body?", flags=[ResponseFlags.BODY_FOUND_OTHER, ResponseFlags.BODY_FOUND_ME]),
-    Dialog("i saw [p] vent in [lm]", max_turns=1, flags=[ResponseFlags.EMERGENCY_MEET_ME]),
+    Dialog("[p]", max_turns=0, flags=[RF.BODY_FOUND_OTHER, RF.BODY_FOUND_ME]),
+    Dialog("why was [p] near the body?", flags=[RF.BODY_FOUND_OTHER, RF.BODY_FOUND_ME]),
+    Dialog("i saw [p] vent in [lm]", max_turns=1, flags=[RF.EMERGENCY_MEET_ME]),
     Dialog("vote [p]"),
-    Dialog("why is [p] stalking me?", max_turns=0, flags=[ResponseFlags.EMERGENCY_MEET_ME]),
-    Dialog("self report", flags=[ResponseFlags.SELF_REPORT]),
+    Dialog("why is [p] stalking me?", max_turns=0, flags=[RF.EMERGENCY_MEET_ME]),
+    Dialog("self report", flags=[RF.SELF_REPORT]),
     Dialog("I don't buy [p]'s story", min_turns=1),
 ]
 
@@ -67,7 +67,7 @@ defense = [
     Dialog("i was in [lm]"),
     Dialog("this is my first time", min_turns=3),
     Dialog("my tasks are done"),
-    Dialog("task check", max_turns=0, flags=[ResponseFlags.EMERGENCY_MEET_ME]),
+    Dialog("task check", max_turns=0, flags=[RF.EMERGENCY_MEET_ME]),
     Dialog("i was doing my tasks"),
     Dialog("you weren't there, I was there"),
     Dialog("I was doing my tasks", max_turns=0),
